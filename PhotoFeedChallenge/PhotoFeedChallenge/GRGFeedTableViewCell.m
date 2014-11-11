7571ce7c973aa9c6a32783ee341a98d5eed13899
Greg Gunner <greg@socialsafe.net>
2014-11-11 20:25:41 +0000

    [FIXED] FeedTableViewCell photoImageView now adjusts it's width to accommodate the various device screen widths.

--- PhotoFeedChallenge/PhotoFeedChallenge/GRGFeedTableViewCell.m
+++ PhotoFeedChallenge/PhotoFeedChallenge/GRGFeedTableViewCell.m
@@ -21,16 +21,19 @@
     if (self) {
         [self.contentView setBackgroundColor:[UIColor whiteColor]];
         
-        self.photoImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 20.0f, self.contentView.frame.size.width, 120.0f)];
-        [self.photoImageView setBackgroundColor:[UIColor colorWithWhite:0.8 alpha:1.0]];
+        self.photoImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 20.0f, [[UIScreen mainScreen] bounds].size.width, 120.0f)];
+        [self.photoImageView setBackgroundColor:[UIColor colorWithWhite:0.9 alpha:1.0]];
         [self.photoImageView setOpaque:YES];
         [self.photoImageView setContentMode:UIViewContentModeScaleAspectFill];
         [self.photoImageView setClipsToBounds:YES];
         [self.contentView addSubview:self.photoImageView];
         
-        self.titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, self.contentView.frame.size.width, 20.0f)];
+        CGFloat textInset = 2.0f;
+        self.titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(textInset, 0, [[UIScreen mainScreen] bounds].size.width-textInset, 20.0f)];
         [self.titleLabel setBackgroundColor:self.contentView.backgroundColor];
         [self.titleLabel setOpaque:YES];
+        [self.titleLabel setNumberOfLines:0];
+        [self.titleLabel setLineBreakMode:NSLineBreakByTruncatingTail];
         [self.titleLabel setTextColor:[UIColor blackColor]];
         [self.titleLabel setFont:[UIFont systemFontOfSize:16.0f]];
         [self.contentView addSubview:self.titleLabel];
